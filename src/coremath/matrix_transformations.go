5fa4f18117eef2c52116fb6fb632c21f34d0401f
Anirudh Sundar <anirudh.sundar@gmail.com>
2021-07-27 17:02:40 +0530

    Add scaling and reflection

--- src/coremath/matrix_transformations.go
+++ src/coremath/matrix_transformations.go
@@ -7,3 +7,11 @@ func Translation(x, y, z float64) *Matrix {
 	m.Set(z, 2, 3)
 	return m
 }
+
+func Scaling(x, y, z float64) *Matrix {
+	m := IdentityMatrix(4)
+	m.Set(x, 0, 0)
+	m.Set(y, 1, 1)
+	m.Set(z, 2, 2)
+	return m
+}
--- src/coremath/matrix_transformations_test.go
+++ src/coremath/matrix_transformations_test.go
@@ -31,6 +31,7 @@ func TestTranslation2(t *testing.T) {
 		t.Errorf("Expected Translation of (5, -3, 2) to Point \n%v\n to be \n%v\n, but got \n%v\n", p, expectedTranslation, translated)
 	}
 }
+
 func TestTranslation3(t *testing.T) {
 	tr := coremath.Translation(5, -3, 2)
 	p := coremath.Vector(-3, 4, 5)
@@ -39,5 +40,55 @@ func TestTranslation3(t *testing.T) {
 	check(err, t)
 	if !p.Equal(*translated) {
 		t.Errorf("Expected Translation of (5, -3, 2) to Vector \n%v\n to be \n%v\n, but got \n%v\n", p, p, translated)
+	}
+}
+
+func TestScaling1(t *testing.T) {
+	tr := coremath.Scaling(2, 3, 4)
+	p := coremath.Point(-4, 6, 8)
+
+	scaled, err := tr.TupleMultiply(*p)
+	check(err, t)
+	expectedScaling := coremath.Point(-8, 18, 32)
+	if !expectedScaling.Equal(*scaled) {
+		t.Errorf("Expected Translation of (2, 3, 4) to Point \n%v\n to be \n%v\n, but got \n%v\n", p, expectedScaling, scaled)
+	}
+}
+
+func TestScaling2(t *testing.T) {
+	tr := coremath.Scaling(2, 3, 4)
+	p := coremath.Vector(-4, 6, 8)
+
+	scaled, err := tr.TupleMultiply(*p)
+	check(err, t)
+	expectedScaling := coremath.Vector(-8, 18, 32)
+	if !expectedScaling.Equal(*scaled) {
+		t.Errorf("Expected Translation of (2,3,4) to Vector \n%v\n to be \n%v\n, but got \n%v\n", p, expectedScaling, scaled)
+	}
+}
+
+func TestScaling3(t *testing.T) {
+	tr := coremath.Scaling(2, 3, 4)
+	inv, err := tr.Inverse()
+	check(err, t)
+	p := coremath.Vector(-4, 6, 8)
+
+	translated, err := inv.TupleMultiply(*p)
+	check(err, t)
+	expectedTranslation := coremath.Vector(-2, 2, 2)
+	if !expectedTranslation.Equal(*translated) {
+		t.Errorf("Expected Translation of (2,3,4) to Point \n%v\n to be \n%v\n, but got \n%v\n", p, expectedTranslation, translated)
+	}
+}
+
+func TestReflection(t *testing.T) {
+	tr := coremath.Scaling(-1, 1, 1)
+	p := coremath.Point(2, 3, 4)
+
+	translated, err := tr.TupleMultiply(*p)
+	check(err, t)
+	expectedTranslation := coremath.Point(-2, 3, 4)
+	if !expectedTranslation.Equal(*translated) {
+		t.Errorf("Expected Translation of (-1,1,1) to Point \n%v\n to be \n%v\n, but got \n%v\n", p, expectedTranslation, translated)
 	}
 }
